c93e963d26514bc146150202ebb5c168ab1e3fbc
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-07-12 11:31:11 +0700

    Chapter 04

--- rust-atcoder/src/main.rs
+++ rust-atcoder/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    /* Chapter 03: Hello, world! */
     // New line
     println!();
     // Standard error
     eprintln!("Hello, world3!");
+
+    /* Chapter 04: Arithmetic operation */
+    // Place holder
+    println!("{} {}", 2, "times");
+    // Arithmetic operation
+    println!("2 + 3 = {}", 2 + 3);
+    println!("2 * 3 = {}", 2 * 3);
+    println!("{}", 6.5 / 2.5);
 }
