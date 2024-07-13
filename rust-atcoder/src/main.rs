7d3bb8bbbcca18f887bc69c3cd288953e8d75161
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-07-13 21:50:42 +0700

    Chapter 06

--- rust-atcoder/src/main.rs
+++ rust-atcoder/src/main.rs
@@ -18,4 +18,27 @@ fn main() {
     let length;
     length = 5;
     println!("Area={}", length * length);
+
+    /* Cahpter 06: Literal */
+    // String literal
+    println!("\"Fool,\" said I, \"you do not know\"");
+    println!(
+        "/\\
+\\/"
+    );
+    // Raw string literal
+    println!(r"\\\\\\\\\\");
+    println!(r#""Fool," said I, "you do not know""#);
+    // Integer literal
+    //let big_number: i32;
+    let big_number: i64;
+    big_number = 2147483648;
+    println!("{}", big_number);
+    let big_number2;
+    big_number2 = 2147483648_i64;
+    println!("{}", big_number2);
+    // Floating-point literal
+    let avogadro_constant;
+    avogadro_constant = 6.02e+23;
+    println!("{}", avogadro_constant);
 }
