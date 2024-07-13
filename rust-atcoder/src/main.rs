a7e5cc730d95200649f98d2d6f2e326fd31fb6da
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-07-13 21:27:56 +0700

    Chapter 05

--- rust-atcoder/src/main.rs
+++ rust-atcoder/src/main.rs
@@ -13,4 +13,9 @@ fn main() {
     println!("2 + 3 = {}", 2 + 3);
     println!("2 * 3 = {}", 2 * 3);
     println!("{}", 6.5 / 2.5);
+
+    /* Cahpter 05: Variables and types */
+    let length;
+    length = 5;
+    println!("Area={}", length * length);
 }
