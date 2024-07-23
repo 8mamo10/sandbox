75940af7f5b92ac31ffb9f3edceb20b33702b5be
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-07-23 16:58:35 +0700

    ABC174 B, refine

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     proconio::input! {
         n: usize,
-        d: usize,
+        d: f64,
         v: [(f64, f64); n],
     }
     let mut ans = 0;
     for &(x, y) in &v {
-        if x.hypot(y) <= d as f64 {
+        if x.hypot(y) <= d {
             ans += 1;
         }
     }
