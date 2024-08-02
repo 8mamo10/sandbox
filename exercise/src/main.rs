806483eac036d2ef0a0594e50373326348360655
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-08-02 10:29:05 +0700

    ABC168 A

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,18 +1,11 @@
 fn main() {
     proconio::input! {
         n: usize,
-        mut l: [usize; n],
     }
-    l.sort();
-    let mut ans = 0;
-    for i in 0..l.len() {
-        for j in 0..i {
-            for k in 0..j {
-                if l[i] != l[j] && l[j] != l[k] && l[i] < l[j] + l[k] {
-                    ans += 1;
-                }
-            }
-        }
+    match n % 10 {
+        2 | 4 | 5 | 7 | 9 => println!("hon"),
+        0 | 1 | 6 | 8 => println!("pon"),
+        3 => println!("bon"),
+        _ => unreachable!(),
     }
-    println!("{}", ans);
 }
