a780bcd28a5283da52a916dd363eb51890b658ea
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-08-16 19:24:38 +0900

    Chapter 34

--- rust-atcoder/src/main.rs
+++ rust-atcoder/src/main.rs
@@ -934,6 +934,38 @@ fn main() {
     print_display_and_debug("Hello");
 
     /* Chapter 33: Functions that return references */
+    let vec = vec![2, 4, 7, 8, 6, 3, 5];
+    let slice = &vec[..];
+    let mut inc = slice;
+    for i in 0..slice.len() - 1 {
+        if slice[i] >= slice[i + 1] {
+            inc = &slice[..=i];
+            break;
+        }
+    }
+    println!("{:?}", inc);
+    // lifetime parameter
+    let vec = vec![2, 4, 7, 8, 6, 3, 5];
+    let result = increasing(&vec);
+    println!("{:?}", result);
+
+    let mut four = vec![1, 2, 3, 4];
+    let result;
+    {
+        let three = vec![1, 2, 3];
+        result = longer(&four, &three);
+        assert_eq!(&four, result);
+        println!("{:?}", add(&mut four, &three));
+    }
+    // omission of lifetime parametre
+    let (former, latter) = split_mid(&[3, 1, 4, 1, 5, 2]);
+    println!("{:?} {:?}", former, latter);
+    let (former, latter) = split_mid2(&[3, 1, 4, 1, 5, 2]);
+    println!("{:?} {:?}", former, latter);
+    // static lifetime
+    for i in 0..50 {
+        println!("{}{}", i, ordinal_suffix(i))
+    }
 }
 
 fn fact5() -> i32 {
@@ -1102,4 +1134,51 @@ where
 fn print_display_and_debug<T: std::fmt::Display + std::fmt::Debug>(x: T) {
     println!("{}", x);
     println!("{:?}", x);
+}
+
+fn increasing<'a>(slice: &'a [i32]) -> &'a [i32] {
+    let mut ret: &'a [i32] = slice;
+    for i in 0..slice.len() - 1 {
+        if slice[i] >= slice[i + 1] {
+            ret = &slice[..=i];
+            break;
+        }
+    }
+    ret
+}
+
+fn add<'a>(x: &'a mut [i32], y: &[i32]) -> &'a [i32] {
+    let len = x.len().min(y.len());
+    for i in 0..len {
+        x[i] += y[i];
+    }
+    &x[0..len]
+}
+
+fn longer<'a>(x: &'a [i32], y: &'a [i32]) -> &'a [i32] {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn split_mid<'a>(slice: &'a [i32]) -> (&'a [i32], &'a [i32]) {
+    let mid = slice.len() / 2;
+    (&slice[..mid], &slice[mid..])
+}
+
+fn split_mid2(slice: &[i32]) -> (&[i32], &[i32]) {
+    let mid = slice.len() / 2;
+    (&slice[..mid], &slice[mid..])
+}
+
+fn ordinal_suffix(number: u32) -> &'static str {
+    match (number % 10, number % 100) {
+        (_, 11..=13) => "th",
+        (1, _) => "st",
+        (2, _) => "nd",
+        (3, _) => "rd",
+        _ => "th",
+    }
 }
